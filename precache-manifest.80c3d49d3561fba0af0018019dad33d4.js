736cdf994f84846649f6fb6ff19380ff64ace481
Bunlong <bunlong@Bunlongs-MacBook-Pro.local>
2023-11-01 21:39:46 +1100

    Updates

---
+++ precache-manifest.80c3d49d3561fba0af0018019dad33d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dbcb8ce7d724f3049d7",
+    "url": "/portfolio-react-project/static/css/main.1c69106a.chunk.css"
+  },
+  {
+    "revision": "6dbcb8ce7d724f3049d7",
+    "url": "/portfolio-react-project/static/js/main.6dbcb8ce.chunk.js"
+  },
+  {
+    "revision": "b49c8f1649b9041b493b",
+    "url": "/portfolio-react-project/static/js/1.b49c8f16.chunk.js"
+  },
+  {
+    "revision": "84f56659e00a2f98d869",
+    "url": "/portfolio-react-project/static/js/runtime~main.84f56659.js"
+  },
+  {
+    "revision": "5a6b1a750113cec64433f21ffd6696fe",
+    "url": "/portfolio-react-project/index.html"
+  }
+];
